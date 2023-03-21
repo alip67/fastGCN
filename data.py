minor bug in calculation of spectral embedding

data.py
@@ -270,9 +270,9 @@ def spectral_embedding(data, ncol=0, drp_first=True):
     DA = D.dot(A)
     L = DA.dot(D)
 
-    # X, Y = eigsh(A=L, k=k, which='LM')
-    X = torch.load('eigval_embedding_arxic.pt', map_location=torch.device('cpu'))
-    Y = torch.load('eigvec_embedding_arxic.pt', map_location=torch.device('cpu'))
+    X, Y = eigsh(A=L, k=k, which='LM')
+    # X = torch.load('eigval_embedding_arxic.pt', map_location=torch.device('cpu'))
+    # Y = torch.load('eigvec_embedding_arxic.pt', map_location=torch.device('cpu'))
     X = torch.tensor(X)
     Y = torch.tensor(Y)
 
@@ -282,14 +282,14 @@ def spectral_embedding(data, ncol=0, drp_first=True):
 
     # D = torch.diag(1. / torch.sqrt(deg))
     D  = torch.sparse.spdiags(1/deg.sqrt(), torch.tensor([0]), (data.num_nodes, data.num_nodes))
-    Y = torch.matmul(D, Y)
+    Y = torch.matmul(D, Y) #row_wise multiplication each row of Y with 1/sqrt(deg)
     Y = torch.sub(Y, torch.matmul(deg, Y) / deg.sum())
 
     # D = torch.diag(deg)
     D = torch.sparse.spdiags(deg, torch.tensor([0]), (data.num_nodes, data.num_nodes))
     for j in range(Y.size(1)):
         x = Y[:, j]
-        Y[:,j] = x/torch.sqrt(torch.mul(torch.mul(x, deg), x))
+        Y[:,j] = x/torch.sqrt(torch.matmul(torch.mul(x, deg), x))
 
     if drp_first: Y = Y[:, 1:Y.size(1)]
     constant = torch.matmul(torch.mul(Y[:, 0], deg), Y[:,0])